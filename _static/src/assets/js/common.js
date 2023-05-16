07fd694b5904d57023bae9c58074095a75ae7adc
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-05-17 08:20:08 +0900

    Add: three.jsをnpmでインストール

--- _static/src/assets/js/common.js
+++ _static/src/assets/js/common.js
@@ -1,5 +1,40 @@
-// import jQuery from 'jquery';
-// const $ = jQuery;
+import * as THREE from "three";
 
-// require('./modules/humburgerBtn');
-// require('./modules/mvHeight');
+// ページの読み込みを待つ
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas"),
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(400, 400, 400);
+  const material = new THREE.MeshNormalMaterial();
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    box.rotation.y += 0.01;
+    renderer.render(scene, camera); // レンダリング
+
+    requestAnimationFrame(tick);
+  }
+}
